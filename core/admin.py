d70c0195c8db4ebd2c2e7df5233d95ab5469c7fe
Mohammad Foisal <abbasnurul805@gmail.com>
2024-01-12 18:16:51 +0600

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -7,7 +7,7 @@ admin.site.site_title = "NDITC"
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
+    readonly_fields = ('id',)
     list_display = Member.DisplayFields
     search_fields = Member.SearchFields
     list_filter = Member.FilterFields
-    readonly_fields = ('id',)
